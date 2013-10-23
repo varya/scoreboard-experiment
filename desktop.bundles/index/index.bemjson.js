2c11d88356b33ba7e855db405e354dd203e20b9d
Varya Stepanova <mail@varya.me>
2013-10-23 13:42:44 +0200

    Declaring schedule block in the page file

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -5,26 +5,26 @@
     head: [
         { elem: 'css', url: '_index.css', ie: false },
         { elem: 'css', url: '_index.ie.css', ie: 'gte IE 6' },
-        { block: 'i-jquery', elem: 'core' },
         { elem: 'meta', attrs: { name: 'description', content: '' }}
     ],
     content:[
         {
-            block: 'header',
+            block: 'schedule',
+            js: {
+                modelName: 'schedule'
+            },
             content: [
-                'header content goes here'
-            ]
-        },
-        {
-            block: 'content',
-            content: [
-                'main content'
-            ]
-        },
-        {
-            block: 'footer',
-            content: [
-                'footer content goes here'
+                {
+                    elem: 'title',
+                    content: 'Super puper races'
+                },
+                {
+                    elem: 'events',
+                    mix: {
+                        block: 'schedule',
+                        elem: 'model-field'
+                    }
+                }
             ]
         },
         { block: 'i-jquery', mods: { version: '1.8.3' } },
