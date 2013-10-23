c28342483a63645f762b3679a2923a8f672e5a28
Varya Stepanova <mail@varya.me>
2013-10-23 13:38:09 +0200

    Initial structure from project-stub

---
+++ .bem/make.js
@@ -0,0 +1,38 @@
+/* jshint node:true */
+/* global MAKE */
+
+require('bem-environ/lib/nodes');
+
+//process.env.YENV = 'production';
+
+MAKE.decl('Arch', {
+
+    blocksLevelsRegexp: /^.+?\.blocks/,
+    bundlesLevelsRegexp: /^.+?\.bundles$/,
+
+    libraries: [
+        'bem-bl @ 0.3',
+        'bem-controls @ v1'
+    ]
+
+});
+
+
+MAKE.decl('BundleNode', {
+
+    getTechs: function() {
+
+        return [
+            'bemjson.js',
+            'bemdecl.js',
+            'deps.js',
+            'bemhtml',
+            'js',
+            'css',
+            'ie.css',
+            'html'
+        ];
+
+    }
+
+});
---
+++ desktop.bundles/index/index.bemjson.js
@@ -0,0 +1,32 @@
+({
+    block: 'b-page',
+    title: 'Title of the page',
+    favicon: '/favicon.ico',
+    head: [
+        { elem: 'css', url: '_index.css', ie: false },
+        { elem: 'css', url: '_index.ie.css', ie: 'gte IE 6' },
+        { elem: 'meta', attrs: { name: 'description', content: '' }}
+    ],
+    content:[
+        {
+            block: 'header',
+            content: [
+                'header content goes here'
+            ]
+        },
+        {
+            block: 'content',
+            content: [
+                'main content'
+            ]
+        },
+        {
+            block: 'footer',
+            content: [
+                'footer content goes here'
+            ]
+        },
+        { block: 'i-jquery', mods: { version: '1.8.3' } },
+        { elem: 'js', url: '_index.js' }
+    ]
+})
