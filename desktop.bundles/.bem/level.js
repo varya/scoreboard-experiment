93d462cabe1b1feb3afeb93a21a524cfd6d91a90
Varya Stepanova <mail@varya.me>
2013-10-24 11:53:32 +0200

    Use islands-components and bem-components block libraries

--- desktop.bundles/.bem/level.js
+++ desktop.bundles/.bem/level.js
@@ -11,11 +11,16 @@ exports.getConfig = function() {
                 'bem-bl/blocks-common',
                 'bem-bl/blocks-desktop',
                 'bem-mvc/common.blocks',
-                'bem-mvc/desktop.blocks'
+                'bem-mvc/desktop.blocks',
+                'bem-history/common.blocks'
             ]
             .map(function(path) { return PATH.resolve(environ.LIB_ROOT, path); })
             .concat([
                 'ya-libs/romochka/common.blocks',
+                'libs/bem-components/common.blocks',
+                'ya-libs/islands-components/common.blocks',
+                'libs/bem-components/desktop.blocks',
+                'ya-libs/bem-components/desktop.blocks',
                 'common.blocks',
                 'desktop.blocks'
             ]
